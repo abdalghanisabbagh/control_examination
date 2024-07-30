21abccceb140c31ffb216bddd313cf1539ec1e49
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-30 10:33:54 +0300

    ✨ feat: Enhance user profile model with additional fields
    The changes made to the `user_profile_model.dart` file include the addition of several new fields to the `UserProfileModel` class. These changes were made to enhance the user profile data and provide more detailed information about the user.
    
    The new fields added are:
    - `active`: Indicates if the user is active or not
    - `blbId`: The unique identifier for the user's BLB (Blended Learning Buddy)
    - `cohortID`, `cohortName`: Information about the user's cohort
    - `createdAt`, `createdBy`, `updatedAt`, `updatedBy`: Timestamps and user IDs for creation and updates
    - `email`, `firstName`, `gradeName`, `gradesID`, `religion`, `schoolClassID`, `schoolClassName`, `schoolsID`, `secondLang`, `secondName`, `thirdName`: Additional personal and academic information about the user
    
    These changes were made to provide a more comprehensive and detailed user profile model, enabling the application to handle a wider range of user data and potentially offer more features and functionality related to user management.

--- lib/models/login_response/user_profile_model.dart
+++ lib/models/login_response/user_profile_model.dart
@@ -1,44 +1,110 @@
 class UserProfileModel {
+  int? active;
+
+  int? blbId;
+
+  // ClassRoomResModel? classRoomResModel;
+
+  int? cohortID;
+  String? cohortName;
+  // CohortResModel? cohortResModel;
+  String? createdAt;
+  int? createdBy;
+  String? email;
+  String? firstName;
+  String? gradeName;
+  // GradeResModel? gradeResModel;
+  int? gradesID;
+  int? iD;
+  String? religion;
+  int? schoolClassID;
+  String? schoolClassName;
+  int? schoolsID;
+  String? secondLang;
+  String? secondName;
+  String? thirdName;
+  DateTime? updatedAt;
+  int? updatedBy;
   UserProfileModel({
     this.iD,
-    this.fullName,
-    this.userName,
+    this.blbId,
+    this.gradesID,
+    this.gradeName,
+    this.schoolsID,
+    this.cohortID,
+    this.cohortName,
+    this.schoolClassID,
+    this.schoolClassName,
+    this.firstName,
+    this.secondName,
+    this.thirdName,
+    this.email,
+    this.secondLang,
     this.createdBy,
     this.createdAt,
     this.updatedBy,
     this.updatedAt,
+    // this.gradeResModel,
+    // this.classRoomResModel,
+    // this.cohortResModel,
     this.active,
+    this.religion,
   });
 
   UserProfileModel.fromJson(json) {
     iD = json['ID'];
-    fullName = json['Full_Name'];
-    userName = json['User_Name'];
+    blbId = json['Blb_Id'];
+    gradesID = json['Grades_ID'];
+    schoolsID = json['Schools_ID'];
+    cohortID = json['Cohort_ID'];
+    schoolClassID = json['School_Class_ID'];
+    firstName = json['First_Name'];
+    secondName = json['Second_Name'];
+    thirdName = json['Third_Name'];
+    email = json['Email'];
+    secondLang = json['Second_Lang'];
     createdBy = json['Created_By'];
     createdAt = json['Created_At'];
     updatedBy = json['Updated_By'];
     updatedAt = json['Updated_At'];
-    active = json['Active'];
+    religion = json['Religion'];
+    // gradeResModel =
+    //     json['grades'] == null ? null : GradeResModel.fromJson(json['grades']);
+    // classRoomResModel = json['school_class'] == null
+    //     ? null
+    //     : ClassRoomResModel.fromJson(json['school_class']);
+    // cohortResModel =
+    //     json['cohort'] == null ? null : CohortResModel.fromJson(json['cohort']);
+    // active = json['Active'];
   }
-
-  String? active;
-  String? createdAt;
-  int? createdBy;
-  String? fullName;
-  int? iD;
-  String? updatedAt;
-  int? updatedBy;
-  String? userName;
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['ID'] = iD;
-    data['Full_Name'] = fullName;
-    data['User_Name'] = userName;
+    data['Blb_Id'] = blbId;
+    data['Grades_ID'] = gradesID;
+    data['Schools_ID'] = schoolsID;
+    data['Cohort_ID'] = cohortID;
+    data['School_Class_ID'] = schoolClassID;
+    data['First_Name'] = firstName;
+    data['Second_Name'] = secondName;
+    data['Third_Name'] = thirdName;
+    data['Email'] = email;
+    data['Second_Lang'] = secondLang;
     data['Created_By'] = createdBy;
     data['Created_At'] = createdAt;
     data['Updated_By'] = updatedBy;
     data['Updated_At'] = updatedAt;
+    data['Religion'] = religion;
+    // if (gradeResModel != null) {
+    //   data['grades'] = gradeResModel!.toJson();
+    // }
+    // if (classRoomResModel != null) {
+    //   data['school_class'] = classRoomResModel!.toJson();
+    // }
+    // if (cohortResModel != null) {
+    //   data['cohort'] = cohortResModel!.toJson();
+    // }
     data['Active'] = active;
     return data;
   }
