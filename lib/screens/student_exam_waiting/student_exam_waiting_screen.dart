8a95c296f324c6ef76471fbe2e01fa5dd6ae88a6
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-07-31 11:40:30 +0300

    🎨 feat(student_exam_waiting): Create new screen for student exam waiting
    This change introduces a new `StudentExamWaitingScreen` widget that
    provides a basic Scaffold with no content. This screen will be used
    to display the waiting state for a student taking an exam.

---
+++ lib/screens/student_exam_waiting/student_exam_waiting_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StudentExamWaitingScreen extends StatelessWidget {
+  const StudentExamWaitingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
