fcb8edb7ead8bd53f0e82e6dc87d273896641b00
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-01 11:10:02 +0300

    📦 feat(controllers): Add student exam controller
    Adds the student exam controller to the `controllers.dart` file.
    This new controller will handle the logic for the student exam
    functionality in the application.

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -3,4 +3,5 @@ export 'home/home.dart';
 export 'login_controller.dart';
 export 'profile_controller.dart';
 export 'side_menu_controller.dart';
+export 'student_exam/student_exam.dart';
 export 'websocket_controller.dart';
