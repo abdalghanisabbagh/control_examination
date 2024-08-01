8b620d05d98438e6005c2f81e735261adb511b4f
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-01 13:12:43 +0300

    ✨feat: Add UuidResModel class
    The UuidResModel class is added to the project to represent the response model
    for the UUID API. This model includes properties such as ID, UUID, ExamMissionId,
    student_id, active, CreatedAt, UpdatedAt, Created_by, and Updated_By. The class
    also provides methods to convert the model to and from JSON.

---
+++ lib/models/uuid/uuid_res_model.dart
@@ -0,0 +1,49 @@
+class UuidResModel {
+  int? iD;
+  String? uUID;
+  int? examMissionId;
+  String? studentId;
+  int? active;
+  String? createdAt;
+  String? updatedAt;
+  int? createdBy;
+  String? updatedBy;
+
+  UuidResModel({
+    this.iD,
+    this.uUID,
+    this.examMissionId,
+    this.studentId,
+    this.active,
+    this.createdAt,
+    this.updatedAt,
+    this.createdBy,
+    this.updatedBy,
+  });
+
+  UuidResModel.fromJson(json) {
+    iD = json['ID'];
+    uUID = json['UUID'];
+    examMissionId = json['ExamMissionId'];
+    studentId = json['student_id'];
+    active = json['active'];
+    createdAt = json['CreatedAt'];
+    updatedAt = json['UpdatedAt'];
+    createdBy = json['Created_by'];
+    updatedBy = json['Updated_By'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['ID'] = iD;
+    data['UUID'] = uUID;
+    data['ExamMissionId'] = examMissionId;
+    data['student_id'] = studentId;
+    data['active'] = active;
+    data['CreatedAt'] = createdAt;
+    data['UpdatedAt'] = updatedAt;
+    data['Created_by'] = createdBy;
+    data['Updated_By'] = updatedBy;
+    return data;
+  }
+}
