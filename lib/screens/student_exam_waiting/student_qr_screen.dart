9c8632f2d8248e8860eaa997241fe7deb4d72dd2
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-01 10:21:43 +0300

    ✨ feat(student_qr_screen): Implement student QR code screen
    This commit introduces the `StudentQrScreen` widget, which is responsible for
    displaying the student's QR code and a countdown timer. The QR code contains
    the student's name and their barcode, which can be used for identification
    during the exam. The countdown timer shows the remaining time until the exam
    starts, and when the time reaches 2 minutes, the screen will navigate to the
    next screen (TODO).
    
    The key changes in this commit are:
    
    - Implement the `StudentQrScreen` widget
    - Display the student's QR code using the `PrettyQrView` widget
    - Show a countdown timer using the `SlideCountdown` widget
    - Style the screen using the `ColorManager` and `nunitoSemiBold` and
      `nunitoBold` text styles
    - Retrieve the necessary data from the `ExamMissionController` and
      `ProfileController`

---
+++ lib/screens/student_exam_waiting/student_qr_screen.dart
@@ -0,0 +1,83 @@
+import 'package:control_examination/configurations/constants/assets.dart';
+import 'package:control_examination/controllers/controllers.dart';
+import 'package:control_examination/resource_manager/index.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pretty_qr_code/pretty_qr_code.dart';
+import 'package:slide_countdown/slide_countdown.dart';
+
+class StudentQrScreen extends StatelessWidget {
+  final int _start = DateTime.parse(Get.find<ExamMissionController>()
+          .cachedExamMission!
+          .startTime
+          .toString())
+      .toUtc()
+      .difference(DateTime.now().toUtc())
+      .inSeconds
+      .abs(); // Countdown starting value
+
+  StudentQrScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Student Exam Waiting',
+          style: nunitoSemiBold.copyWith(
+            fontSize: 16,
+            color: ColorManager.white,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: ColorManager.primary,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            Expanded(
+              flex: 5,
+              child: PrettyQrView.data(
+                data:
+                    '${Get.find<ProfileController>().cachedUserProfile?.firstName} ${Get.find<ProfileController>().cachedUserProfile?.secondName} ${Get.find<ProfileController>().cachedUserProfile?.thirdName}\n ${Get.find<ProfileController>().cachedUserProfile?.barcodesResModel?.barcodes?.first.barcode}',
+                decoration: const PrettyQrDecoration(
+                  image: PrettyQrDecorationImage(
+                    image: AssetImage(
+                      Assets.assetsLogosNisLogo,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+            Expanded(
+              flex: 3,
+              child: SlideCountdown(
+                onChanged: (value) => value.inSeconds <= 2 * 60
+                    ? null // TODO: validate time and go to next screen
+                    : null,
+                duration: Duration(seconds: _start),
+                style: nunitoBold.copyWith(
+                  color: ColorManager.white,
+                  fontSize: 100,
+                ),
+                decoration: BoxDecoration(
+                  color: ColorManager.bgSideMenu,
+                ),
+              ),
+            ),
+            const Spacer(
+              flex: 2,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
