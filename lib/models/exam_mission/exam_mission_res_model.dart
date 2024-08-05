c876808c1dfd33ad8c6cdda1358f45daa84d6c86
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-05 09:16:58 +0300

    ✨ feat(ExamMissionResModel): Add new properties
    Adds new properties to the `ExamMissionResModel` class to handle
    additional data fields returned from the API. These include `month`,
    `year`, `period`, `duration`, `pdf`, and `pdfV2`. The corresponding
    getter and setter methods have also been added to the class.

--- lib/models/exam_mission/exam_mission_res_model.dart
+++ lib/models/exam_mission/exam_mission_res_model.dart
@@ -8,9 +8,29 @@ class ExamMission {
   String? startTime;
   String? endTime;
   int? duration;
+  String? month;
+
+  String? pdf;
+
+  String? pdfV2;
+
+  bool? period;
+
+  String? year;
+
   SubjectResModel? subjects;
 
-  ExamMission({this.examRoomHasExamMission, this.iD, this.subjects});
+  ExamMission({
+    this.examRoomHasExamMission,
+    this.iD,
+    this.subjects,
+    this.month,
+    this.year,
+    this.period,
+    this.duration,
+    this.pdf,
+    this.pdfV2,
+  });
 
   ExamMission.fromJson(json) {
     if (json['exam_room_has_exam_mission'] != null) {
@@ -20,6 +40,18 @@ class ExamMission {
       });
     }
     iD = json['ID'];
+    month = json['Month'];
+
+    year = json['Year'];
+
+    period = json['Period'];
+
+    duration = json['duration'];
+
+    pdf = json['pdf'];
+
+    pdfV2 = json['pdf_V2'];
+
     startTime = json['start_time'];
     endTime = json['end_time'];
     duration = json['duration'];
@@ -33,6 +65,11 @@ class ExamMission {
     data['start_time'] = startTime;
     data['end_time'] = endTime;
     data['duration'] = duration;
+    data['Month'] = month;
+    data['Year'] = year;
+    data['Period'] = period;
+    data['pdf'] = pdf;
+    data['pdf_V2'] = pdfV2;
     if (examRoomHasExamMission != null) {
       data['exam_room_has_exam_mission'] =
           examRoomHasExamMission!.map((v) => v.toJson()).toList();
