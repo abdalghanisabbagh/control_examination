10ff5c33e0c45e17a5d5b2c6d2bdb75bdb63a95e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-06 14:31:29 +0300

    📝 feat(web): add right-click event handler
    Adds a listener to the HTML document to handle right-click events and
    prevent the default context menu from appearing. This allows for custom
    actions to be performed on right-click events in the web version of the
    application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
 import 'package:control_examination/main_app.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Add a listener to the document to handle right-clicks.
+  html.document.onContextMenu.listen((event) {
+    event.preventDefault(); // Prevent the default context menu
+    // Custom action here
+  });
 
   await Future.wait([
     Hive.initFlutter(),
