241cde5f9cbd3b64cda6a309b03cede7ae1ded15
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-08 15:12:38 +0300

    ✨ feat: Update ExamMission model fields
    This change updates the ExamMission model to better reflect the structure of the data. The
    following changes were made:
    
    - Added `controlMissionResModel`, `duration`, and `endTime` fields
    - Reordered and grouped related fields together
    - Removed unnecessary line breaks
    
    These changes improve the readability and maintainability of the model.

--- lib/models/exam_mission/exam_mission_res_model.dart
+++ lib/models/exam_mission/exam_mission_res_model.dart
@@ -3,25 +3,20 @@ import '../exam_has_exam_mission/exam_room_has_exam_mission.dart';
 import '../subject/subject_res_model.dart';
 
 class ExamMission {
-  List<ExamRoomHasExamMission>? examRoomHasExamMission;
-  int? iD;
-  String? startTime;
-  String? endTime;
-  int? duration;
-  String? month;
-
-  String? pdf;
-
-  String? pdfV2;
-
-  bool? period;
-
-  String? year;
-
-  SubjectResModel? subjects;
-
   ControlMissionResModel? controlMissionResModel;
 
+  int? duration;
+
+  String? endTime;
+  List<ExamRoomHasExamMission>? examRoomHasExamMission;
+  int? iD;
+  String? month;
+  String? pdf;
+  String? pdfV2;
+  bool? period;
+  String? startTime;
+  SubjectResModel? subjects;
+  String? year;
   ExamMission({
     this.examRoomHasExamMission,
     this.iD,
@@ -36,7 +31,6 @@ class ExamMission {
     this.endTime,
     this.controlMissionResModel,
   });
-
   ExamMission.fromJson(json) {
     if (json['control_mission'] != null) {
       controlMissionResModel =
