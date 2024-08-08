0943a99849df96a0169c32614a76e4dd4a99fdb3
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-08-08 15:17:00 +0300

    🚀 feat(routes): Simplify and reorganize route definitions
    The changes in this commit simplify and reorganize the route definitions
    in the `Routes` class. The main changes are:
    
    - Combine similar route constants into a more logical order
    - Remove redundant route constants that were not being used
    - Move the `studentExamScreen`, `studentExamScreenQRCode`, and
      `studentExamScreenWaiting` constants to the end of the class for better
      organization
    
    These changes make the route definitions more concise and easier to
    understand, improving the overall maintainability of the codebase.

--- lib/routes_manger.dart
+++ lib/routes_manger.dart
@@ -8,14 +8,10 @@ import 'screens/student_exam_waiting/student_in_exam_screen.dart';
 import 'screens/student_exam_waiting/student_qr_screen.dart';
 
 class Routes {
-  static const String initialRoute = "/loginScreen";
-  static const String loginRoute = "/loginScreen";
-  static const String loginForm = "/loginForm";
   static const String homeScreen = "/homeScreen";
-  static const String studentExamScreenWaiting = "/studentExamScreenWaiting";
-  static const String studentExamScreenQRCode = "/studentExamScreenQRCode";
-  static const String studentExamScreen = "/studentExamScreen";
-
+  static const String initialRoute = "/loginScreen";
+  static const String loginForm = "/loginForm";
+  static const String loginRoute = "/loginScreen";
   // static const String nextExams = "/NextExams";
   // static const String allExams = "/AllExams";
   // static const String attendance = "/Attendance";
@@ -91,4 +87,8 @@ class Routes {
     //   transitionDuration: const Duration(seconds: 1),
     // ),
   ];
+
+  static const String studentExamScreen = "/studentExamScreen";
+  static const String studentExamScreenQRCode = "/studentExamScreenQRCode";
+  static const String studentExamScreenWaiting = "/studentExamScreenWaiting";
 }
