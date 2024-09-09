0b5b049c0bf175d88a13e92dd13e3d2a3b405609
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2024-09-09 09:18:15 +0300

    🐛 Fix: Incorrect class name `A.wo` in `main.dart.js`
    The changes in this commit fix an incorrect class name in the `main.dart.js` file. The class `A.wn` was incorrectly referenced as `A.wo`, which could lead to runtime errors. This commit ensures that the correct class name is used throughout the codebase.

--- .firebase/control-examination/hosting/flutter_service_worker.js
+++ .firebase/control-examination/hosting/flutter_service_worker.js
@@ -65,14 +65,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "5d3b3bbeec4ce6123a648b681b799df6
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "7d844e596d4c0da5e4d0b6b35c196b97",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "d6106b7b58bc59194d2a74c3aa687f41",
+"flutter_bootstrap.js": "2e060a911d86f29b5af83cebc40f0fed",
 "icons/Icon-192.png": "db9fb010616d67905d18a3c517479f4e",
 "icons/Icon-512.png": "4f1685b6f43d89b05e30955afc5f7aae",
 "icons/Icon-maskable-192.png": "db9fb010616d67905d18a3c517479f4e",
 "icons/Icon-maskable-512.png": "4f1685b6f43d89b05e30955afc5f7aae",
 "index.html": "4d80b8f3e04fd1f2868889b73d71e1ee",
 "/": "4d80b8f3e04fd1f2868889b73d71e1ee",
-"main.dart.js": "eb0df15c2d5588282133ce50d3227f0c",
+"main.dart.js": "c3b2d838d0526818ebd427fd430d7202",
 "manifest.json": "60126968cdf38d535992dddee3bdbd48",
 "version.json": "b4e868fb895bf867103ee711d47b05f6"};
 // The application shell files that are downloaded before a service worker can
